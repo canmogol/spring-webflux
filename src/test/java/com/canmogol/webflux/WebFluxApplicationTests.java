abd28015fd4fa60d7006fca7b191919527714daa
Can MOGOL <canmogol@gmail.com>
2021-07-08 20:33:00 +0200

    Add initial files.

---
+++ src/test/java/com/canmogol/webflux/WebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canmogol.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
